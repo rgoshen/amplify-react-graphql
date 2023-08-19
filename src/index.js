cb110399d47733d4c363fad1d68eade31e5c8c77
Richard Goshen <rick.goshen@gmail.com>
2023-08-19 13:21:23 -0700

    Dev (#5)
    * add backend setup for user authentication
    
    * add auth

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
