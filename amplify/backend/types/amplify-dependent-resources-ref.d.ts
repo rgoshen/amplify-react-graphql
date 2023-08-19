99575b80e76823461c5955b145d37e00ade35d43
Richard Goshen <rick.goshen@gmail.com>
2023-08-19 14:14:12 -0700

    add api
    - add graphQL api
    - add frontend code to handle calls to api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlbbd5a182": {
       "AppClientID": "string",
